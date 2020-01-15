1cc968f878b7d248db8ad091bdfe79ae648164dd
sangmai <sangmai350@gmail.com>
2020-01-15 13:52:38 +0700

    change port

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as bodyParser from "body-parser";
 import * as helmet from "helmet";
 import * as cors from "cors";
 import routes from "./routes";
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 
 //Connects to the Database -> then starts the express
 createConnection()
@@ -20,8 +21,8 @@ createConnection()
     //Set all routes from routes folder
     app.use("/", routes);
 
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("Server started on port 3000!");
+    app.listen(server_port, () => {
+      console.log(`Server started on port ${server_port}`);
     });
   })
   .catch(error => console.log(error));
